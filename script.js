679c952f136861b4c92529c29b99598e78e3f536
Ark <arbaazkhanark23@gmail.com>
2023-01-21 23:28:07 +0530

    Styles Done

--- script.js
+++ script.js
@@ -6,5 +6,18 @@ document.querySelectorAll("img")[0].setAttribute('src',PlayerDice1);
 
 
 let player2=Math.round(Math.random()*6)+1;
-const PlayerDice2=`img/dice${player2}.png`
+const PlayerDice2=`/img/dice${player2}.png`
 document.querySelectorAll("img")[1].setAttribute('src',PlayerDice2);
+
+
+
+
+////////// Winner Logic ///////////
+
+if(player1>player2){
+    document.querySelector('h1').innerHTML="The Winner is Player 1 🥳🥳🥳";
+}else if(player1<player2){
+    document.querySelector('h1').innerHTML="The Winner is Player 2 🥳🥳🥳";
+}else{
+    document.querySelector('h1').innerHTML="The Match is Draw!!!!!!!!"
+}
