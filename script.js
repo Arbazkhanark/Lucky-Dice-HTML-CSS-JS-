226292e1f17719b1d6cfdf85af1965604433ca60
Ark <arbaazkhanark23@gmail.com>
2023-01-21 21:16:51 +0530

    Dice Images Added by using JAVASCRIPT

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 let player1=Math.round(Math.random()*6)+1;
 
 // alert(player1);
+const PlayerDice1=`img/dice${player1}.png`;
+document.querySelectorAll("img")[0].setAttribute('src',PlayerDice1);
 
-const firstDice=`img/dice${player1}.png`;
 
-document.querySelectorAll("img")[0].setAttribute('src',firstDice);
+let player2=Math.round(Math.random()*6)+1;
+const PlayerDice2=`img/dice${player2}.png`
+document.querySelectorAll("img")[1].setAttribute('src',PlayerDice2);
